3efbb62aeafa17eb377232ebd9a3837805e1c4cb
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-25 23:26:24 +0800

    feat: create student migration

---
+++ database/migrations/2024_09_25_151222_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('class')->index();
+            $table->unsignedTinyInteger('level')->index();
+            $table->string('parent_contact_no');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropIndex(['class', 'level']);
+        });
+        Schema::dropIfExists('students');
+    }
+};
