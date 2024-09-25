05c9cc16a048e433a93cd1b325307cefcfff9add
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-26 01:36:36 +0800

    feat: create a scratch view student list

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -4,9 +4,37 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {{ __('You are logged in!') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            <div class="card">
+                <div class="card-body">
+                    <form action="">
+                        <div class="mb-3">
+                            <div class="d-flex justify-content-between align-items-center">
+                                <label for="uploadFile" class="form-label">Upload File</label>
+                                <a class="btn btn-primary" href="#" role="button">Download Excel Template</a>
+                            </div>
+                            <div class="mb-3 mt-2">
+                                <input type="file" class="form-control" id="uploadFile">
+                            </div>
+                            <div class="d-flex gap-2 mt-2">
+                                <button type="submit" class="btn btn-outline-primary w-100">Upload</button>
+                                <button type="button" class="btn btn-outline-danger w-100">Cancel</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="card mt-2">
+                <form action="">
+                    <div class="input-group p-3">
+                        <input type="text" class="form-control" placeholder="Search Student" value="">
+                        <button class="btn btn-success" type="submit">Search</button>
+                    </div>
+                </form>
+
+                <div class="card-body">
+                    <p class="text-center text-muted">No student data available.</p>
+                </div>
             </div>
         </div>
     </div>
