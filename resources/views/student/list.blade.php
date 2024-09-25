5e3de74e71a86ce6c245bf448e8964c6a4d7f580
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-26 00:13:24 +0800

    feat: create student index with list view

---
+++ resources/views/student/list.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                {{ __('You are logged in!') }}
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+
