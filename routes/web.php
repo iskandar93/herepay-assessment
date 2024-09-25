8f133e3849cc21e4ff6a4b7cc44ad5f1f5d73dcf
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-26 00:11:52 +0800

    feat(web): add student index route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(StudentController::class)->prefix('student')->group(function () {
+    Route::get('/list', 'index')->name('student.list');
 });
 
 Auth::routes();
