ee2366707119046436bc7de392cc385f4ee98d3e
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-27 06:41:54 +0800

    feat: implement middleware auth

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(StudentController::class)->prefix('student')->group(function () {
-    Route::get('/list', 'index')->name('student.list');
-    Route::get('/template', 'studentDataTemplate')->name('student.template');
-    Route::post('/upload', 'upload')->name('student.upload');
-    Route::get('/delete/{student}', 'delete')->name('student.delete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::controller(StudentController::class)->prefix('student')->group(function () {
+        Route::get('/list', 'index')->name('student.list');
+        Route::get('/template', 'studentDataTemplate')->name('student.template');
+        Route::post('/upload', 'upload')->name('student.upload');
+        Route::get('/delete/{student}', 'delete')->name('student.delete');
+    });
 });
 
 Auth::routes();
