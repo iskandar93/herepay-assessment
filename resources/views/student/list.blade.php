cbaa9cb8e28c54a231594aa690e23f4ebb5babc9
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-26 13:01:52 +0800

    feat: added validation success import and error invalid file

--- resources/views/student/list.blade.php
+++ resources/views/student/list.blade.php
@@ -9,12 +9,20 @@
                     <form method="POST" action="{{ route('student.upload') }}" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
+                            @if (session('status'))
+                                <div class="alert {{ session('alert-type') }}">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
                             <div class="d-flex justify-content-between align-items-center">
                                 <label for="uploadFile" class="form-label">Upload File</label>
                                 <a class="btn btn-primary" href="{{ route('student.template') }}" role="button">Download Excel Template</a>
                             </div>
                             <div class="mb-3 mt-2">
-                                <input type="file" class="form-control" name="file" id="uploadFile">
+                                <input type="file" class="form-control @error('file') is-invalid @enderror" name="file" id="uploadFile">
+                                @error('file')
+                                    <div class="mt-1 alert alert-danger" id="file-error">{{ $message }}</div>
+                                @enderror
                             </div>
                             <div class="d-flex gap-2 mt-2">
                                 <button type="submit" class="btn btn-outline-primary w-100">Upload</button>
