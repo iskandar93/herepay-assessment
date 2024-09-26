95b5d5c8b570f81baf518399f13fa26f288f0aea
Iskandar Isman <iskandarisman93@gmail.com>
2024-09-26 12:55:48 +0800

    feat: create validation upload file

---
+++ app/Http/Requests/UploadRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UploadRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'file' => 'required|mimes:xlsx,csv, xls',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'file.required' => 'Please insert file',
+            'file.mimes' => 'The file must be a file of type: xlsx, csv, xls.',
+        ];
+    }
+}
